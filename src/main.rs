dd6ed6660bc803b262e9bf304f77c18423fbee74
Alex Alves <jhosealex@gmail.com>
2021-03-02 19:38:04 -0300

    Iniciando conexão com o banco de dados no main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use myhealth::configuration::get_configuration;
 use myhealth::routes::run;
+use sqlx::PgPool;
+use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
