f2f0d873b9e62f74079c3eeba204df8882791d04
Alex Alves <jhosealex@gmail.com>
2021-03-10 18:58:01 -0300

    Ajustando configuração com o banco

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
 use myhealth::configuration::get_configuration;
-use myhealth::routes::run;
-use sqlx::PgPool;
-use std::net::TcpListener;
+use myhealth::routes::Application;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let application = Application::build(configuration).await?;
     
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    application.run_until_stopped().await?;
+    Ok(())
 }
--- src/routes/index.rs
+++ src/routes/index.rs
@@ -1,12 +1,55 @@
 
+use crate::configuration::{DatabaseSettings, Settings};
 use actix_web::{web, App, HttpServer};
 use actix_web::dev::Server;
+use actix_web::web::Data;
 use crate::controllers::{usuario, usuarios};
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
-    let db_pool = web::Data::new(db_pool);
+pub struct Application {
+    port: u16,
+    server: Server,
+}
+
+impl Application {
+    pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
+        let connection_pool = get_connection_pool(&configuration.database)
+            .await
+            .expect("Failed to connect to Postgres.");
+
+        let address = format!(
+            "{}:{}",
+            configuration.application.host, configuration.application.port
+        );
+        let listener = TcpListener::bind(&address)?;
+        let port = listener.local_addr().unwrap().port();
+        let server = run(listener, connection_pool)?;
+
+        Ok(Self { port, server })
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+
+    pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
+}
+
+pub async fn get_connection_pool(configuration: &DatabaseSettings) -> Result<PgPool, sqlx::Error> {
+    PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_with(configuration.with_db())
+        .await
+}
+
+fn run(
+    listener: TcpListener, 
+    db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = Data::new(db_pool);
 
     let server = HttpServer::new(move || {
         App::new()
