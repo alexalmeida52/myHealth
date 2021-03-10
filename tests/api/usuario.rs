166c766648e5d02efb8d96918ca8e3887703ff6e
Alex Alves <jhosealex@gmail.com>
2021-03-10 20:50:20 -0300

    Ajustando teste de inserção de usuário

--- tests/api/usuario.rs
+++ tests/api/usuario.rs
@@ -1,18 +1,23 @@
 use crate::helpers::create_app;
+use std::collections::HashMap;
 
 #[actix_rt::test]
-async fn subscribe_returns_a_200() {
+async fn usuario_returns_a_200() {
     let app = create_app().await;
     let client = reqwest::Client::new();
-    let body = "name=da%20silva&email=joao_da_silva%40gmail.com";
-
+    let mut map = HashMap::new();
+    map.insert("nome", "Josefa Barbosa");
+    map.insert("email", "josefabarbosa@gmail.com");
+    map.insert("senha", "12345");
+    map.insert("tipo", "paciente");
+    
     let response = client
         .post(&format!("{}/usuario", &app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(body)
+        .json(&map)
         .send()
         .await
         .expect("Failed to execute request.");
-
+    println!("{}", response.status().as_u16());
     assert_eq!(200, response.status().as_u16());
 }
