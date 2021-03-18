9be6972d3a89c0ff7bcf77f3f0af9785fe29ab24
Alex Alves <jhosealex@gmail.com>
2021-03-18 11:34:06 -0300

    Teste para a rota de atualizar usuario

--- tests/api/usuario.rs
+++ tests/api/usuario.rs
@@ -1,14 +1,15 @@
 use crate::helpers::create_app;
 use std::collections::HashMap;
 use myhealth::controllers::{UsuarioDB, UsuarioId};
+use uuid::Uuid;
 
 #[actix_rt::test]
 async fn create_user_returns_a_200() {
     let app = create_app().await;
     let client = reqwest::Client::new();
     let mut map = HashMap::new();
-    map.insert("nome", "Josefa Barbosa");
-    map.insert("email", "josefabarbosa@gmail.com");
+    map.insert("nome", "José Alex");
+    map.insert("email", "jhosealex@gmail.com");
     map.insert("senha", "12345");
     map.insert("tipo", "paciente");
     
@@ -27,9 +28,58 @@ async fn create_user_returns_a_200() {
         .await
         .expect("Failed to fetch saved usuario.");
 
-    assert_eq!(data.nome, "Josefa Barbosa");
+    assert_eq!(data.nome, "José Alex");
     assert_eq!(data.tipo, "paciente");
-    assert_eq!(data.email, "josefabarbosa@gmail.com");
+    assert_eq!(data.email, "jhosealex@gmail.com");
+}
+
+#[actix_rt::test]
+async fn update_user_returns_a_200() {
+    let app = create_app().await;
+    let client = reqwest::Client::new();
+    let mut map = HashMap::new();
+    map.insert("nome", "José Alex");
+    map.insert("email", "jhosealex@gmail.com");
+    map.insert("senha", "12345");
+    map.insert("tipo", "paciente");
+    
+    let response = client
+        .post(&format!("{}/usuarios", &app.address))
+        .header("Content-Type", "application/json")
+        .json(&map)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+        
+    assert_eq!(200, response.status().as_u16());
+
+    let create_response: UsuarioId = response.json().await.unwrap();
+
+    let mut user_update = HashMap::new();
+    user_update.insert("nome", "Alex Alves");
+    user_update.insert("email", "jhosealex@gmail.com");
+    user_update.insert("tipo", "profissional");
+
+    let response_2 = client
+        .put(&format!("{}/usuarios/{}", &app.address, create_response.id))
+        .header("Content-Type", "application/json")
+        .json(&user_update)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response_2.status().as_u16());
+
+    let user_uuid: Uuid = Uuid::parse_str(&create_response.id).unwrap();
+
+    let data = sqlx::query!("SELECT email, nome, tipo FROM usuario WHERE id = $1", user_uuid)
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved usuario.");
+
+    assert_eq!(data.nome, "Alex Alves");
+    assert_eq!(data.tipo, "profissional");
+    assert_eq!(data.email, "jhosealex@gmail.com");
 }
 
 #[actix_rt::test]
